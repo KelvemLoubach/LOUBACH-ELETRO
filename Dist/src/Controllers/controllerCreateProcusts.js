6f44e439cf1cfba3973bad4e6372206e1ebe4de6
Kelvem Loubach <kelvemloubach@outlook.com>
2023-05-09 19:05:03 -0300

    Deploy 2

--- Dist/src/Controllers/controllerCreateProcusts.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createProducts = void 0;
-const createProducts = async (req, res) => {
-    res.send('Olá, você está na rota createProducts');
-};
-exports.createProducts = createProducts;
